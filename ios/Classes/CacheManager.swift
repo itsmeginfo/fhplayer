db86cd6af224c4e1d7ea186095bc3d962e360742
ItsMeGinfo <53050728+itsmeginfo@users.noreply.github.com>
2024-01-26 16:06:26 +0100

    patch on player

--- ios/Classes/CacheManager.swift
+++ ios/Classes/CacheManager.swift
@@ -3,8 +3,9 @@ import Cache
 import HLSCachingReverseProxyServer
 import GCDWebServer
 import PINCache
+import AVFoundation
 
-@objc public class CacheManager: NSObject {
+@objc public class CacheManager: NSObject, AVAssetResourceLoaderDelegate {
 
     // We store the last pre-cached CachingPlayerItem objects to be able to play even if the download
     // has not finished.
@@ -87,16 +88,67 @@ import PINCache
     }
     
     ///Gets caching player item for normal playback.
-    @objc public func getCachingPlayerItemForNormalPlayback(_ url: URL, cacheKey: String?, videoExtension: String?, headers: Dictionary<NSObject,AnyObject>) -> AVPlayerItem? {
-        let mimeTypeResult = getMimeType(url:url, explicitVideoExtension: videoExtension)
-        if (mimeTypeResult.1 == "application/vnd.apple.mpegurl"){
-            let reverseProxyURL = server?.reverseProxyURL(from: url)!
-            let playerItem = AVPlayerItem(url: reverseProxyURL!)
-            return playerItem
-        } else {
-            return getCachingPlayerItem(url, cacheKey: cacheKey, videoExtension: videoExtension, headers: headers)
-        }
+    @objc public func getCachingPlayerItemForNormalPlayback(_ url: URL, cacheKey: String?, videoExtension: String?, headers: [NSObject:AnyObject], certificateUrl: String?, licenseUrl: String?) -> AVPlayerItem? {
+        let mimeTypeResult = getMimeType(url: url, explicitVideoExtension: videoExtension)
+
+            if mimeTypeResult.1 != "application/vnd.apple.mpegurl" {
+                // Reverse proxy URL létrehozása
+                if let reverseProxyURL = server?.reverseProxyURL(from: url) {
+                    // AVURLAsset létrehozása
+                    var bearerToken = ""
+                    if let authValue = headers["Authorization" as NSObject] {
+                        let components = authValue.components(separatedBy: " ")
+                        
+                        
+                        if components.count >= 2 {
+                            bearerToken = components[1]
+                        }
+                    }
+                     let asset = AVURLAsset(url: reverseProxyURL)
+                    // DRM-kezelés hozzáadása a Swift kódban, ha szükséges
+                    if let certificateUrl = certificateUrl, !certificateUrl.isEmpty {
+                        if let authValue = headers["Authorization" as NSObject] {
+                            let components = authValue.components(separatedBy: " ")
+                            var bearerToken = ""
+
+                            if components.count >= 2 {
+                                bearerToken = components[1]
+                            }
+
+                            guard let certificateNSURL = URL(string: certificateUrl) else {
+                                // Kezelés, ha a URL nem érvényes
+                                return nil
+                            }
+
+                            var licenseNSURL: URL?
+
+                            if let licenseUrl = licenseUrl, !licenseUrl.isEmpty {
+                                licenseNSURL = URL(string: licenseUrl)
+                            } else {
+                                // Kezeljük az NSNull vagy nil értéket, például adjunk neki alapértelmezett értéket
+                                print("A licenseUrl nil vagy NSNull, alapértelmezett érték használva")
+                                // Adj hozzá alapértelmezett URL-t vagy kezelje a helyzetet más módon
+                            }
+
+                            let loaderDelegate = BetterPlayerEzDrmAssetsLoaderDelegate(certificateURL: certificateNSURL, licenseURL: licenseNSURL, bearerToken: bearerToken)
+
+                            // Asset resource loader delegate hozzárendelése
+                            asset.resourceLoader.setDelegate(loaderDelegate, queue: DispatchQueue(label: "streamQueue"))
+                        }
+                    }
+
+                    // AVPlayerItem létrehozása
+                    let playerItem = AVPlayerItem(asset: asset)
+                    return playerItem
+                }
+            } else {
+                // DRM-kezelés Swift kódban, ha nem a cache részágban van
+                return getCachingPlayerItem(url, cacheKey: cacheKey, videoExtension: videoExtension, headers: headers)
+            }
+
+        return nil
     }
+
     
 
     // Get a CachingPlayerItem either from the network if it's not cached or from the cache.
@@ -118,7 +170,12 @@ import PINCache
                     NSLog("Cache error: couldn't find mime type for url: \(url.absoluteURL). For this URL cache didn't work and video will be played without cache.")
                     playerItem = CachingPlayerItem(url: url, cacheKey: _key, headers: headers)
                 } else {
-                    playerItem = CachingPlayerItem(data: data!, mimeType: mimeTypeResult.1, fileExtension: mimeTypeResult.0)
+                    // ISM Smooth Streaming típus esetén
+                    if mimeTypeResult.1 == "application/vnd.ms-sstr+xml" {
+                        playerItem = CachingPlayerItem(url: url, cacheKey: _key, headers: headers)
+                    } else {
+                        playerItem = CachingPlayerItem(data: data!, mimeType: "application/vnd.apple.mpegurl", fileExtension: "m3u8")
+                    }
                 }
             } else {
                 // The file is not cached.
@@ -126,9 +183,11 @@ import PINCache
                 self._existsInStorage = false
             }
         }
+        
         playerItem.delegate = self
         return playerItem
     }
+
     
     // Remove all objects
     @objc public func clearCache(){
@@ -136,65 +195,44 @@ import PINCache
         self._preCachedURLs = Dictionary<String,CachingPlayerItem>()
     }
     
-    private func getMimeType(url: URL, explicitVideoExtension: String?) -> (String,String){
+    private func getMimeType(url: URL, explicitVideoExtension: String?) -> (String, String) {
         var videoExtension = url.pathExtension
-        if (explicitVideoExtension != nil){
-            videoExtension = explicitVideoExtension!
+        if let explicitVideoExtension = explicitVideoExtension {
+            videoExtension = explicitVideoExtension
         }
         var mimeType = ""
-        switch (videoExtension){
-        case "m3u":
-            mimeType = "application/vnd.apple.mpegurl"
-        case "m3u8":
+
+        switch videoExtension.lowercased() {
+        case "m3u", "m3u8":
             mimeType = "application/vnd.apple.mpegurl"
         case "3gp":
             mimeType = "video/3gpp"
-        case "mp4":
+        case "mp4", "m4a", "m4p", "m4b", "m4r", "m4v":
             mimeType = "video/mp4"
-        case "m4a":
-            mimeType = "video/mp4"
-        case "m4p":
-            mimeType = "video/mp4"
-        case "m4b":
-            mimeType = "video/mp4"
-        case "m4r":
-            mimeType = "video/mp4"
-        case "m4v":
-            mimeType = "video/mp4"
-        case "m1v":
-            mimeType = "video/mpeg"
-        case "mpg":
-            mimeType = "video/mpeg"
-        case "mp2":
-            mimeType = "video/mpeg"
-        case "mpeg":
-            mimeType = "video/mpeg"
-        case "mpe":
-            mimeType = "video/mpeg"
-        case "mpv":
+        case "m1v", "mpg", "mp2", "mpeg", "mpe", "mpv":
             mimeType = "video/mpeg"
         case "ogg":
             mimeType = "video/ogg"
-        case "mov":
-            mimeType = "video/quicktime"
-        case "qt":
+        case "mov", "qt":
             mimeType = "video/quicktime"
         case "webm":
             mimeType = "video/webm"
-        case "asf":
-            mimeType = "video/ms-asf"
-        case "wma":
-            mimeType = "video/ms-asf"
-        case "wmv":
+        case "asf", "wma", "wmv":
             mimeType = "video/ms-asf"
         case "avi":
             mimeType = "video/x-msvideo"
         default:
             mimeType = ""
         }
-        
+
+        if mimeType.isEmpty {
+            // ISM Smooth Streaming MIME típus
+            mimeType = "application/vnd.ms-sstr+xml"
+        }
+
         return (videoExtension, mimeType)
     }
+
     
     ///Checks wheter pre cache is supported for given url.
     @objc public func isPreCacheSupported(url: URL, videoExtension: String?) -> Bool{
@@ -223,4 +261,133 @@ extension CacheManager: CachingPlayerItemDelegate {
         NSLog("Error when downloading the file %@", error as NSError);
         self.completionHandler?(false)
     }
+}
+
+
+class BetterPlayerEzDrmAssetsLoaderDelegate: NSObject, AVAssetResourceLoaderDelegate {
+    var assetId: String = ""
+    var license: Data?
+
+    let DEFAULT_LICENSE_SERVER_URL = "https://fps.ezdrm.com/api/licenses/"
+    var certificateURL: URL
+    var licenseURL: URL?
+    var bearerToken: String
+
+    init(certificateURL: URL, licenseURL: URL?, bearerToken: String) {
+        self.certificateURL = certificateURL
+        self.licenseURL = licenseURL
+        self.bearerToken = bearerToken
+        super.init()
+    }
+
+    typealias DataCompletionBlock = (Data?, Error?) -> Void
+
+    func getContentKeyAndLeaseExpiryFromKeyServerModule(with requestBytes: Data, assetId: String, contentId: String, completion: @escaping DataCompletionBlock) {
+        var finalLicenseURL: URL
+        if let licenseURL = licenseURL {
+            finalLicenseURL = licenseURL
+        } else {
+            finalLicenseURL = URL(string: DEFAULT_LICENSE_SERVER_URL)!
+        }
+
+        var request = URLRequest(url: finalLicenseURL)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-type")
+
+        if !bearerToken.isEmpty {
+            let authorizationHeader = "Bearer " + bearerToken
+            request.setValue(authorizationHeader, forHTTPHeaderField: "Authorization")
+        }
+
+        let stringBody = "spc=\(requestBytes.base64EncodedString())&assetId=\(contentId)"
+        let body = stringBody.data(using: .utf8)
+        request.httpBody = body
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(nil, error)
+            } else if let data = data {
+                if let str = String(data: data, encoding: .utf8),
+                   let startRange = str.range(of: "<ckc>"),
+                   let endRange = str.range(of: "</ckc>") {
+                    let startIndex = startRange.upperBound
+                    let endIndex = endRange.lowerBound
+                    let strippedString = String(str[startIndex..<endIndex])
+
+                    if let decodedData = Data(base64Encoded: strippedString)
+                    {
+                            completion(decodedData, nil)
+                    }
+                    
+                } else {
+                    completion(nil, nil) // Handle parsing error
+                }
+            }
+        }
+
+        dataTask.resume()
+    }
+
+    func getAppCertificate(_ string: String) -> Data? {
+        do {
+            let certificate = try Data(contentsOf: certificateURL)
+            license = certificate
+            return certificate
+        } catch {
+            return nil
+        }
+    }
+
+    func resourceLoader(_ resourceLoader: AVAssetResourceLoader, shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest) -> Bool {
+        let assetURI = loadingRequest.request.url
+        let str = assetURI?.absoluteString ?? ""
+        let contentId = assetURI?.host ?? ""
+        let mySubstring = String(str.suffix(36))
+        assetId = mySubstring
+        let scheme = assetURI?.scheme ?? ""
+
+        guard scheme == "skd" else {
+            return false
+        }
+
+        do {
+            guard let certificate = try getAppCertificate(assetId) else {
+                throw NSError(domain: NSURLErrorDomain, code: NSURLErrorClientCertificateRejected, userInfo: nil)
+            }
+
+            let requestBytes = try loadingRequest.streamingContentKeyRequestData(forApp: certificate, contentIdentifier: str.data(using: .utf8)!, options: nil)
+            let passthruParams = "?customdata=\(assetId)"
+
+            getContentKeyAndLeaseExpiryFromKeyServerModule(with: requestBytes, assetId: assetId, contentId: contentId) { (data, error) in
+                if let error = error {
+                    // Handle the error
+                    print("Error: \(error)")
+                } else if let data = data {
+                    let dataRequest = loadingRequest.dataRequest
+                    let requestedRange = NSRange(location: Int(dataRequest!.requestedOffset), length: data.count)
+                    let dataInRange = data.subdata(in: Range(requestedRange)!)
+                    dataRequest!.respond(with: dataInRange)
+                    loadingRequest.finishLoading()
+                }
+            }
+        } catch {
+            loadingRequest.finishLoading(with: NSError(domain: NSURLErrorDomain, code: NSURLErrorUnknown, userInfo: nil))
+            return true
+        }
+
+        return true
+    }
+
+    func resourceLoader(_ resourceLoader: AVAssetResourceLoader, shouldWaitForRenewalOfRequestedResource renewalRequest: AVAssetResourceRenewalRequest) -> Bool {
+        return shouldWaitForLoadingOfRequestedResource(resourceLoader, loadingRequest: renewalRequest as AVAssetResourceLoadingRequest)
+    }
+
+    func shouldWaitForLoadingOfRequestedResource(_ resourceLoader: AVAssetResourceLoader, loadingRequest: AVAssetResourceLoadingRequest) -> Bool {
+        // A logika, amit alkalmazni szeretnél
+        // ...
+        return true
+    }
+
+
 }
